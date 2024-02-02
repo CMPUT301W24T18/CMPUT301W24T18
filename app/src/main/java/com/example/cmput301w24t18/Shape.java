b53bca779975e156a9b82a0aadb519f823a3c0be
Aryan-Rusia-2 <110370121+Aryan-Rusia-2@users.noreply.github.com>
2024-02-02 13:03:37 -0700

    Got it finally!

--- app/src/main/java/com/example/cmput301w24t18/Shape.java
+++ app/src/main/java/com/example/cmput301w24t18/Shape.java
@@ -3,5 +3,5 @@ package com.example.cmput301w24t18;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "Purple";
+    private String color = "Blue";
 }
